7b98e3161d3585954d63ddf1872cf6c8463b264f
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-11-13 17:40:12 -0300

    animacao flutterLogo

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animações complexas',
+      home: LogoApp(),
     );
   }
 }
@@ -18,11 +19,48 @@ class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return AnimatedLogo(animation);
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo(Animation<double> animation) : super(listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Center(
+      child: Container(
+        child: FlutterLogo(),
+        width: animation.value,
+        height: animation.value,
+      ),
     );
   }
 }
